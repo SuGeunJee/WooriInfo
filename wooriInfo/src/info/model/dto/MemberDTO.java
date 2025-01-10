b94600081f47511cd52cc978d87e1581635fcc87
letmeloveyou82 <shineanl04@gmail.com>
2025-01-10 16:35:49 +0900

    [Fix] Change toString format in MemberDTO

--- wooriInfo/src/info/model/dto/MemberDTO.java
+++ wooriInfo/src/info/model/dto/MemberDTO.java
@@ -23,17 +23,17 @@ public class MemberDTO {
 		StringBuilder builder = new StringBuilder();
 		builder.append("1. 노트북 번호 : ");
 		builder.append(laptopNumber);
-		builder.append(" 2. 이름 : ");
+		builder.append("\n2. 이름 : ");
 		builder.append(name);
-		builder.append(" 3. 주소 : ");
+		builder.append("\n3. 주소 : ");
 		builder.append(address);
-		builder.append(" 4. 등하교 메이트 구함 여부 (O: 원함, X: 원하지 않음) : ");
+		builder.append("\n4. 등하교 메이트 구함 여부 (O: 원함, X: 원하지 않음) : ");
 		builder.append(mateStatus);
-		builder.append(" 5. 전화번호 : ");
+		builder.append("\n5. 전화번호 : ");
 		builder.append(phoneNumber);
-		builder.append(" 6. 2025년 목표 : ");
+		builder.append("\n6. 2025년 목표 : ");
 		builder.append(goal);
-		builder.append(" 7. 희망하는 스터디 분야 : ");
+		builder.append("\n7. 희망하는 스터디 분야 : ");
 		builder.append(desiredStudy);
 		return builder.toString();
 	}
