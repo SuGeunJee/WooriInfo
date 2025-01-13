b69d39f0618954162ce82b11d0af42d7d46fc50d
letmeloveyou82 <shineanl04@gmail.com>
2025-01-13 14:44:17 +0900

    [Fix] Update CommuteMateDTO to include new field

--- wooriInfo/src/info/model/dto/CommuteMateDTO.java
+++ wooriInfo/src/info/model/dto/CommuteMateDTO.java
@@ -10,14 +10,17 @@ import lombok.Setter;
 public class CommuteMateDTO {
 	private String laptopNumber; // 노트북 번호 (고유한 식별자)
     private String name; // 이름
+    private String address; // 주소
     
     @Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append("1. 노트북 번호 : ");
 		builder.append(laptopNumber);
-		builder.append(" 2. 이름 : ");
+		builder.append("\n2. 이름 : ");
 		builder.append(name);
+		builder.append("\n3. 주소 : ");
+		builder.append(address);
 		return builder.toString();
 	}
 }
